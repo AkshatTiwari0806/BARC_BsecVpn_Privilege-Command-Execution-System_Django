572d3913f47414c4c84ca781f568aafb2fac71c3
AkshatTiwari0806 <aksmaxsteel@gmail.com>
2023-07-03 10:54:28 +0530

    5th commit

--- Barc_main/settings.py
+++ Barc_main/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+ACCESS_TOKEN = '08062002'
 
 # Application definition
 
--- Barcapp/views.py
+++ Barcapp/views.py
@@ -5,55 +5,68 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
+commands_file_path = os.path.join(settings.BASE_DIR, 'Barcapp', 'commands.json')
+with open(commands_file_path) as json_file:
+    data = json.load(json_file)
+
+
+
+from functools import wraps
+
+def token_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        token = request.META.get('HTTP_AUTHORIZATION')
+        if token == settings.ACCESS_TOKEN:
+            return view_func(request, *args, **kwargs)
+        else:
+            return JsonResponse({"status": "failure", "message": "Invalid token", "status_code": 401})
+    return _wrapped_view
+
+
+
+
+@token_required
 @csrf_exempt
 def add_user(request):
     try:
         if request.method == 'POST':
             username = request.POST.get('username')
-            print(request.path)
             if username is None:
-                return JsonResponse({"status": "failure", "message": "Missing 'username' parameter", "status_code": 400})
-            commands_file_path = os.path.join(settings.BASE_DIR, 'Barcapp', 'commands.json')
-            with open(commands_file_path) as json_file:
-                data = json.load(json_file)
-                for command in data[0]:
-                    if command['name'] == request.path and len(username) <= command['parameters'][0]['length'] and isinstance(username, str) == True:
-                        output = execute_command(command['mapped_command'])
-                        print(output)
-                        if output['status_code'] == 0:
-                            return JsonResponse({"status": "success", "message": output['output'], "status_code": 200})
-                        else:
-                            return JsonResponse({"status": "failure", "message": output['error'], "status_code": 500})
-            return JsonResponse({"status": "failure", "message": "Invalid command", "status_code": 400})
+                return JsonResponse({"success": "FALSE", "message": "Missing 'username' parameter", "status_code": 400})
+            for command in data[0]:
+                if command['name'] == request.path and len(username) <= command['parameters'][0]['length'] and isinstance(username, str) == True:
+                    output = execute_command(command['mapped_command'])
+                    print(output)
+                    if output['status_code'] == 0:
+                        return JsonResponse({"success": "TRUE", "message": output['output'], "status_code": 200})
+                    else:
+                        return JsonResponse({"success": "FALSE", "message": output['error'], "status_code": 500})
+            return JsonResponse({"success": "FALSE", "message": "Invalid command", "status_code": 400})
         else:
-            return JsonResponse({"status": "failure", "message": "Invalid request method", "status_code": 400})
+            return JsonResponse({"success": "FALSE", "message": "Invalid request method", "status_code": 400})
     except:
         return JsonResponse({"Message": "Something went wrong"})
 
 @csrf_exempt
+@token_required
 def add_email(request):
-    try:
-        if request.method == 'POST':
-            useremail = request.POST.get('useremail')
-            print(request.path)
-            if useremail is None:
-                return JsonResponse({"status": "failure", "message": "Missing 'user-email' parameter", "status_code": 400})
-            commands_file_path = os.path.join(settings.BASE_DIR, 'Barcapp', 'commands.json')
-            with open(commands_file_path) as json_file:
-                data = json.load(json_file)
-                for command in data[1]:
-                    if command['name'] == request.path and len(useremail) <= command['parameters'][0]['length'] and isinstance(useremail, str) == True:
-                        output = execute_command(command['mapped_command'])
-                        print(output)
-                        if output['status_code'] == 0:
-                            return JsonResponse({"status": "success", "message": output['output'], "status_code": 200})
-                        else:
-                            return JsonResponse({"status": "failure", "message": output['error'], "status_code": 500})
-            return JsonResponse({"status": "failure", "message": "Invalid command", "status_code": 400})
-        else:
-            return JsonResponse({"status": "failure", "message": "Invalid request method", "status_code": 400})
-    except:
-        return JsonResponse({"Message": "Something went wrong"})
+    if request.method == 'POST':
+        useremail = request.POST.get('useremail')
+        if useremail is None:
+            return JsonResponse({"success": "FALSE", "message": "Missing 'user-email' parameter", "status_code": 400})
+        for command in data[1]:
+            if command['name'] == request.path and len(useremail) <= command['parameters'][0]['length'] and isinstance(useremail, str) == True:
+                output = execute_command(command['mapped_command'])
+                print(output)
+                if output['status_code'] == 0:
+                    return JsonResponse({"success": "TRUE", "message": output['output'], "status_code": 200})
+                else:
+                    return JsonResponse({"success": "FALSE", "message": output['error'], "status_code": 500})
+            return JsonResponse({"success": "FALSE", "message": "Invalid command", "status_code": 400})
+    else:
+        return JsonResponse({"success": "FALSE", "message": "Invalid request method", "status_code": 400})
+
     
 
 @csrf_exempt
@@ -61,23 +74,19 @@ def add_password(request):
     try:
         if request.method == 'POST':
             userpassword = request.POST.get('userpassword')
-            print(request.path)
             if userpassword is None:
-                return JsonResponse({"status": "failure", "message": "Missing 'password' parameter", "status_code": 400})
-            commands_file_path = os.path.join(settings.BASE_DIR, 'Barcapp', 'commands.json')
-            with open(commands_file_path) as json_file:
-                data = json.load(json_file)
-                for command in data[2]:
-                    if command['name'] == request.path and len(userpassword) <= command['parameters'][0]['length'] and isinstance(userpassword, str) == True:
-                        output = execute_command(command['mapped_command'])
-                        print(output)
-                        if output['status_code'] == 0:
-                            return JsonResponse({"status": "success", "message": output['output'], "status_code": 200})
-                        else:
-                            return JsonResponse({"status": "failure", "message": output['error'], "status_code": 500})
-            return JsonResponse({"status": "failure", "message": "Invalid command", "status_code": 400})
+                return JsonResponse({"success": "FALSE", "message": "Missing 'password' parameter", "status_code": 400})
+            for command in data[2]:
+                if command['name'] == request.path and len(userpassword) <= command['parameters'][0]['length'] and isinstance(userpassword, str) == True:
+                    output = execute_command(command['mapped_command'])
+                    print(output)
+                    if output['status_code'] == 0:
+                        return JsonResponse({"success": "TRUE", "message": output['output'], "status_code": 200})
+                    else:
+                        return JsonResponse({"success": "FALSE", "message": output['error'], "status_code": 500})
+            return JsonResponse({"success": "FALSE", "message": "Invalid command", "status_code": 400})
         else:
-            return JsonResponse({"status": "failure", "message": "Invalid request method", "status_code": 400})
+            return JsonResponse({"success": "FALSE", "message": "Invalid request method", "status_code": 400})
     except:
         return JsonResponse({"Message": "Something went wrong"})
     
